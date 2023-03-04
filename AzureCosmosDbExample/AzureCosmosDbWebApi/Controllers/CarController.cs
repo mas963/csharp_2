7bc8dc407ee1eb4055243119ee06d066617cc808
yasar <masendgj7@gmail.com>
2023-03-04 22:57:32 +0300

    progressbar, azurecosmos

---
+++ AzureCosmosDbExample/AzureCosmosDbWebApi/Controllers/CarController.cs
@@ -0,0 +1,52 @@
+﻿using AzureCosmosDbWebApi.Models;
+using AzureCosmosDbWebApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureCosmosDbWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CarController : ControllerBase
+{
+    public readonly ICarCosmosService _carCosmosService;
+
+    public CarController(ICarCosmosService carCosmosService)
+    {
+        _carCosmosService = carCosmosService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var sqlCosmosQuery = "SELECT * FROM Car";
+        var result = await _carCosmosService.Get(sqlCosmosQuery);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Car newCar)
+    {
+        newCar.Id = Guid.NewGuid().ToString();
+        var result = await _carCosmosService.AddAsync(newCar);
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Put(Car carToUpdate)
+    {
+        var result = await _carCosmosService.Update(carToUpdate);
+
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(string id, string make)
+    {
+        await _carCosmosService.Delete(id, make);
+
+        return Ok();
+    }
+}
---
+++ ConsoleProgressBarExample/ConsoleProgressBarExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using ShellProgressBar;
+
+const int totalTicks = 10;
+var options = new ProgressBarOptions
+{
+    ProgressCharacter = '─',
+    ProgressBarOnBottom = true
+};
+using (var pbar = new ProgressBar(totalTicks, "Initial message", options))
+{
+    pbar.Tick(); //will advance pbar to 1 out of 10.
+    //we can also advance and update the progressbar text
+    pbar.Tick("Step 2 of 10");
+    pbar.Tick("Step 3 of 10");
+    pbar.Tick("Step 4 of 10");
+}
